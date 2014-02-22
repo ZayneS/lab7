adcb3eb49b24c0701e8413aa37d800102a842343
ZayneS <zheng.shen05@gmail.com>
2014-02-21 22:39:39 -0800

    finished the lab7

--- routes/project.js
+++ routes/project.js
@@ -5,6 +5,7 @@ exports.projectInfo = function(req, res) { 
 
   // query for the specific project and
   // call the following callback
+  models.Project.find({ "_id" : projectID}).exec(afterQuery);
 
   function afterQuery(err, projects) {
     if(err) console.log(err);
@@ -13,15 +14,41 @@ exports.projectInfo = function(req, res) { 
 }
 
 exports.addProject = function(req, res) {
+  // get the form_data as posted from the submit button
   var form_data = req.body;
   console.log(form_data);
 
+  var post_data = {
+                    "title": form_data.project_title,
+                    "date": form_data.date,
+                    "summary": form_data.summary,
+                    "image": form_data.image_url
+                  };
+
+  var newPost = new models.Project(post_data);
+  console.log(newPost);
+
   // make a new Project and save it to the DB
   // YOU MUST send an OK response w/ res.send();
+  newPost.save(afterSaving);
+
+  function afterSaving(err, project){
+    if (err) {console.log(err); res.send(500);}
+    console.log(project);
+    res.redirect('/'); // redirect to home
+  }
+
 }
 
 exports.deleteProject = function(req, res) {
   var projectID = req.params.id;
+
+
+  models.Project.find({ "_id" : projectID}).remove().exec(afterDeleting);
+
+  function afterDeleting(err, removed){
+    res.send();
+  }
 
   // find the project and remove it
   // YOU MUST send an OK response w/ res.send();
